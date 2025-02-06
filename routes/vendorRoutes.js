90755c4111738d3caec69f5c16e08369c17596d2
SahithiKatakam <katakamsahithi1@gmail.com>
2025-02-06 12:11:31 +0530

    Vendor registration is done

---
+++ routes/vendorRoutes.js
@@ -0,0 +1,9 @@
+const vendorController = require('../controllers/vendorController');
+
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/register', vendorController.vendorRegister);
+
+module.exports = router;
