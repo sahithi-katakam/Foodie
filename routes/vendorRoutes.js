ebd339e021082d5b28d3294a90470e60b5bda2fd
SahithiKatakam <katakamsahithi1@gmail.com>
2025-02-11 06:25:31 +0530

    Vendor Login

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -5,5 +5,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/register', vendorController.vendorRegister);
+router.post('/login', vendorController.vendorLogin);
 
 module.exports = router;
